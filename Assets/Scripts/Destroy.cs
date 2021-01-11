1b8746d70503c1531b29d6710e4582fc466138cb
Tanguy <tanguy.geraedts4@gmail.com>
2021-01-11 16:29:59 +0400

    idk what im doing

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -13,6 +13,8 @@ public class Destroy : MonoBehaviour
     public GameObject deathPrefab;
     private GameObject myPlat;
 
+    private int height = 14;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.gameObject.tag.Equals("Death"))
@@ -21,17 +23,17 @@ public class Destroy : MonoBehaviour
             switch (random)
             {
                 default:
-                    myPlat = (GameObject) Instantiate(platformPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (14 + Random.Range(0.2f, 1f))), Quaternion.identity);
+                    myPlat = (GameObject) Instantiate(platformPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (height + Random.Range(-0.2f, 1f))), Quaternion.identity);
                     break;
-                case 1:
-                    myPlat = (GameObject) Instantiate(springPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (14 + Random.Range(0.2f, 1f))), Quaternion.identity);
+                case 12:
+                    myPlat = (GameObject) Instantiate(springPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (height + Random.Range(0.2f, 1f))), Quaternion.identity);
                     break;
                 case 2:
-                    myPlat = (GameObject) Instantiate(cloudPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (14 + Random.Range(0.2f, 1f))), Quaternion.identity);
+                    myPlat = (GameObject) Instantiate(cloudPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (height + Random.Range(0.2f, 1f))), Quaternion.identity);
                     break;
                 case 3:
-                    myPlat = (GameObject) Instantiate(deathPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (14 + Random.Range(0.2f, 1f))), Quaternion.identity);
-                    myPlat = (GameObject) Instantiate(platformPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (14 + Random.Range(0.2f, 1f))), Quaternion.identity);
+                    myPlat = (GameObject) Instantiate(deathPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (height + Random.Range(0.2f, 1f))), Quaternion.identity);
+                    myPlat = (GameObject) Instantiate(platformPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (height + Random.Range(0.2f, 1f))), Quaternion.identity);
                     
                     break;
 
@@ -39,4 +41,5 @@ public class Destroy : MonoBehaviour
         }
         Destroy(other.gameObject);
     }
+
 }
